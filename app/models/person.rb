7614fb4d623a09e639b2ba3259567b4fea8e96e3
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-03-29 16:28:31 -0300

    using LIKE operator for search

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ApplicationRecord
   paginates_per 10
 
   def self.search(search)
-    person = Person.where(first_name: search)
+    person = Person.where("lower(first_name) LIKE ?", "%#{search.downcase}%")
     if person.count == 0 #no result
       nil
     end
