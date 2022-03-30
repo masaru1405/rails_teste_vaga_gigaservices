e7b78a53ae29acaedc9778c26ed80e306dabd9cd
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-03-30 18:05:00 -0300

    add rspec and add command to drop, create, migrate in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,24 @@
-response = RestClient.get 'https://randomuser.me/api/?format=json&results=30&inc=gender,name,email,picture&nat=br&seed=giga'
-json = JSON.parse response
+if Rails.env.development?
 
-if !json.nil?
-   json["results"].each do |i|
-      Person.create!(
-         gender: "#{i['gender']}", 
-         title: "#{i['name']['title']}", 
-         first_name: "#{i['name']['first']}", 
-         last_name: "#{i['name']['last']}", 
-         email: "#{i['email']}", 
-         picture: "#{i['picture']['large']}"
-      )
+   %x(rails db:drop db:create db:migrate) 
+
+   response = RestClient.get 'https://randomuser.me/api/?format=json&results=30&inc=gender,name,email,picture&nat=br&seed=giga'
+   json = JSON.parse response
+
+   if !json.nil?
+      json["results"].each do |i|
+         Person.create!(
+            gender: "#{i['gender']}", 
+            title: "#{i['name']['title']}", 
+            first_name: "#{i['name']['first']}", 
+            last_name: "#{i['name']['last']}", 
+            email: "#{i['email']}", 
+            picture: "#{i['picture']['large']}"
+         )
+      end
+      puts "Banco populado com sucesso!"
+   else
+      puts "Erro ao buscar os dados da API."
    end
-   puts "Successfully"
-else
-   puts "error fetching data"
+
 end
