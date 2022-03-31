f4947fc0bceaade29ef8b326e8ab1a6125030829
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-03-30 21:59:40 -0300

    change code in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,23 @@
-if Rails.env.development? or Rails.env.test?
 
-   %x(rails db:drop db:create db:migrate) 
+%x(rails db:drop db:create db:migrate) 
 
-   response = RestClient.get 'https://randomuser.me/api/?format=json&results=30&inc=gender,name,email,picture&nat=br&seed=giga'
-   json = JSON.parse response
+response = RestClient.get 'https://randomuser.me/api/?format=json&results=30&inc=gender,name,email,picture&nat=br&seed=giga'
+json = JSON.parse response
 
-   if !json.nil?
-      json["results"].each do |i|
-         Person.create!(
-            gender: "#{i['gender']}", 
-            title: "#{i['name']['title']}", 
-            first_name: "#{i['name']['first']}", 
-            last_name: "#{i['name']['last']}", 
-            email: "#{i['email']}", 
-            picture: "#{i['picture']['large']}"
-         )
-      end
-      puts "Banco populado com sucesso!"
-   else
-      puts "Erro ao buscar os dados da API."
+if !json.nil?
+   json["results"].each do |i|
+      Person.create!(
+         gender: "#{i['gender']}", 
+         title: "#{i['name']['title']}", 
+         first_name: "#{i['name']['first']}", 
+         last_name: "#{i['name']['last']}", 
+         email: "#{i['email']}", 
+         picture: "#{i['picture']['large']}"
+      )
    end
-
+   puts "Banco populado com sucesso!"
+else
+   puts "Erro ao buscar os dados da API."
 end
+
+
